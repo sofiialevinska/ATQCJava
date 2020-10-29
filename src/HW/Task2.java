8d23ae167210e2a8fc57040385226c11279b25cf
Sofiia Levinska <sofialevinska@gmail.com>
2020-10-29 12:22:26 +0200

    object Person is added

--- src/HW/Task2.java
+++ src/HW/Task2.java
@@ -5,13 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Task2 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println ("What is your name?");
-        String name = bf.readLine();
-        System.out.println ("Where are you live, " + name + "?");
-        String address = bf.readLine();
 
-        System.out.println(name + " you live in " + address);
+    public static class Person {
+
+        private static String name;
+        private static String address;
+
+        public static void ReadFromConsole() throws IOException {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("What is your name?");
+            name = bf.readLine();
+            System.out.println("Where are you live, " + name + "?");
+            address = bf.readLine();
+        }
+
+        public static void WriteToConsole() {
+            System.out.println(name + " you live in " + address);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+    Person readwrite = new Person();
+    readwrite.ReadFromConsole();
+    readwrite.WriteToConsole();
     }
 }

cef6b411de15d8ac2e27968f2694f13f96e682e1
Sofiia Levinska <sofialevinska@gmail.com>
2020-10-29 12:12:18 +0200

    object Circle is added; classes with circle functionality are added

--- src/HW/Task1.java
+++ src/HW/Task1.java
@@ -5,14 +5,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Task1 {
-    public static void main(String[] args) throws IOException {
+
+    public static double ReadRadius () throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Please enter radius: ");
         double radius = Double.parseDouble(bf.readLine());
-        double pi = 3.14;
+        return radius;
+    }
 
-        System.out.println("Circle perimeter is " + (2*pi*radius));
-        System.out.println("Circle area is: " + pi*radius*radius);
+    public static class Circle {
+        private double pi = 3.14;
+        private double radius;
 
+        public void setRadius(double radius) {
+            this.radius = radius;
+        }
+
+        public void Perimeter () {
+            System.out.println("Circle perimeter is " + String.format("%.2f",2*pi*radius));
+        }
+
+        public void Area () {
+            System.out.println("Circle area is " + String.format("%.2f",pi*radius*radius));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Please enter radius: ");
+        double radius = ReadRadius();
+
+        Circle circle = new Circle();
+        circle.setRadius(radius);
+        circle.Perimeter();
+        circle.Area();
     }
 }

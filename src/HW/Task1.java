c77cffd1c068ef2dd20d784b95251198b53a4736
Sofiia Levinska <sofialevinska@gmail.com>
2020-10-31 18:16:03 +0200

    Object Circle created

--- src/HW/Task1.java
+++ src/HW/Task1.java
@@ -6,26 +6,41 @@ import java.io.InputStreamReader;
 
 public class Task1 {
 
-    public static double ReadRadius () throws IOException {
+    /**
+     * Method for reading Radius value from Console
+     */
+    public static double ReadRadius() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         double radius = Double.parseDouble(bf.readLine());
         return radius;
     }
 
+    /**
+     * Class Circle for creating Object Circle
+     */
     public static class Circle {
         private double pi = 3.14;
         private double radius;
 
+        /**
+         * Circle Radius Setter
+         */
         public void setRadius(double radius) {
             this.radius = radius;
         }
 
-        public void Perimeter () {
-            System.out.println("Circle perimeter is " + String.format("%.2f",2*pi*radius));
+        /**
+         * Method for calculating Circle Perimeter
+         */
+        public void Perimeter() {
+            System.out.println("Circle perimeter is " + String.format("%.2f", 2 * pi * radius));
         }
 
-        public void Area () {
-            System.out.println("Circle area is " + String.format("%.2f",pi*radius*radius));
+        /**
+         * Method for calculating Circle Area
+         */
+        public void Area() {
+            System.out.println("Circle area is " + String.format("%.2f", pi * radius * radius));
         }
     }
 

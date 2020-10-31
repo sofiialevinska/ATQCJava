66cb888bef4ab05ff9776e6eee4d7cf6437641d5
Sofiia Levinska <sofialevinska@gmail.com>
2020-10-31 15:32:19 +0200

    Object Number is added

--- src/HW/Task88.java
+++ src/HW/Task88.java
@@ -7,48 +7,100 @@ import java.math.RoundingMode;
 import java.util.HashSet;
 
 public class Task88 {
+
+    public static class Number {
+        private static int n;
+        private static String str;
+
+        /**
+         * Constructor Number
+         */
+        public Number() {
+            this.n = n;
+        }
+
+        /**
+         * a) Check if number n^2 contains digit 3
+         */
+        public void N2Contains3() {
+            int n2 = n * n;
+            String strn2 = Integer.toString(n2);
+            if (strn2.contains("3")) {
+                System.out.println("Number n^2 contains digit 3");
+            } else {
+                System.out.println("Number n^2 DOES NOT contain digit 3");
+            }
+        }
+
+        /**
+         * b) Print digits of n in the reverse direction
+         */
+
+        public void Reverse() {
+            System.out.print("Digits of n in the reverse direction: ");
+            while (n > 9) {
+                int symbol = n - ((n / 10) * 10);
+                n = n / 10;
+                System.out.print(symbol);
+            }
+            System.out.println(n);
+        }
+
+        /**
+         * c) Changed first and last digits in number n
+         */
+
+        public void ChangeFirstLastDigit() {
+            System.out.print("Changed first and last digits in number n: ");
+            str = Integer.toString(n);
+            char[] chararr = new char[str.length()];
+            chararr = str.toCharArray();
+            if (chararr.length < 2) System.out.println(str);
+            else {
+                System.out.print(chararr[str.length() - 1]);
+                for (int i = 1; i < str.length() - 1; i++) {
+                    System.out.print(chararr[i]);
+                }
+                System.out.println(chararr[0]);
+            }
+        }
+
+        /**
+         * d) Add 1 in the beginning and in the end of number n
+         */
+
+        public void AddFirstLastDigit(int i) {
+            System.out.print("Add 1 in the beginning and in the end of number n: ");
+            System.out.print(i + str + i);
+        }
+    }
+
+
     public static void main(String[] args) throws IOException {
+
+        /** Reading number n from Console */
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Please enter value for n: ");
         int n = Integer.parseInt(br.readLine());
-        int nc= n;
 
-        char c;
+        Number number = new Number();
 
         /** a) Check if number n^2 contains digit 3*/
 
-        int n2= n*n;
-        String str = Integer.toString(n2);
-        if (str.contains("3")) System.out.println("Number n^2 contains digit 3");
-        else System.out.println("Number n^2 DOES NOT contain digit 3");
+        number.N2Contains3();
 
         /** b) Print digits of n in the reverse direction*/
-        System.out.print("Digits of n in the reverse direction: ");
-        while (n>9) {
-            int symbol = n-((n/10)*10);
-            n=n/10;
-            System.out.print(symbol);
-        }
-        System.out.println(n);
+
+        number.Reverse();
 
         /** c) Changed first and last digits in number n*/
 
-        System.out.print("Changed first and last digits in number n: ");
-        String strn= Integer.toString(nc);
-        char [] chararr = new char[strn.length()];
-        chararr = strn.toCharArray ();
-        if (chararr.length<2) System.out.println(strn);
-        else {
-            System.out.print(chararr[strn.length() - 1]);
-            for (int i = 1; i < strn.length() - 1; i++) {
-                System.out.print(chararr[i]);
-            }
-            System.out.println(chararr[0]);
-        }
+        number.ChangeFirstLastDigit();
 
         /** d) Add 1 in the beginning and in the end of number n*/
 
         System.out.print("Add 1 in the beginning and in the end of number n: ");
-        System.out.print("1"+strn+"1");
+        number.AddFirstLastDigit(1);
     }
 }

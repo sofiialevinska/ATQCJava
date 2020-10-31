8da64fe8828a71a0854f971e58d24528e5193620
Sofiia Levinska <sofialevinska@gmail.com>
2020-10-31 18:18:34 +0200

    Object Person created

--- src/HW/Task2.java
+++ src/HW/Task2.java
@@ -6,11 +6,17 @@ import java.io.InputStreamReader;
 
 public class Task2 {
 
+    /**
+     * Class Person
+     */
     public static class Person {
 
         private static String name;
         private static String address;
 
+        /**
+         * Method for reading name and address from Console
+         */
         public static void ReadFromConsole() throws IOException {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("What is your name?");
@@ -19,13 +25,17 @@ public class Task2 {
             address = bf.readLine();
         }
 
+        /**
+         * Method for writing message with name and address to Console
+         */
         public static void WriteToConsole() {
             System.out.println(name + " you live in " + address);
         }
     }
+
     public static void main(String[] args) throws IOException {
-    Person readwrite = new Person();
-    readwrite.ReadFromConsole();
-    readwrite.WriteToConsole();
+        Person readwrite = new Person();
+        readwrite.ReadFromConsole();
+        readwrite.WriteToConsole();
     }
 }

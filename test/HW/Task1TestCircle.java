770957efbbd0a5ac114de3f8a64c3164c1f6f392
Sofiia Levinska <sofialevinska@gmail.com>
2020-11-02 18:43:53 +0200

    JUnit added

--- test/HW/Task1TestCircle.java
+++ test/HW/Task1TestCircle.java
@@ -1,30 +1,29 @@
 package HW;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
 public class Task1TestCircle {
 
-//    @Before
-//    public void setUp() throws Exception {
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//    }
+    private Task1.Circle circle;
+
+    @Before
+    public void setUp() {
+        circle = new Task1.Circle(5.7);
+    }
 
     @Test
     public void TestPerimeter () {
-        Task1.Circle circle = new Task1.Circle(5.7);
-        assertTrue(circle.Perimeter()=="35.80");
+        Assert.assertEquals ("Error", circle.Perimeter(), "35.80");
     }
 
     @Test
     public void TestArea () {
-        Task1.Circle circle = new Task1.Circle(5.7);
-        assertEquals ("102.02", circle.Area());
+        Assert.assertEquals ("Error", circle.Area(), "102.02");
     }
 }

677fbc74910f0bc14dcb6edae343c5bd78f71f40
Sofiia Levinska <sofialevinska@gmail.com>
2020-11-09 16:21:48 +0200

    Comments edited

--- src/HW/Task1Circle.java
+++ src/HW/Task1Circle.java
@@ -1,14 +1,28 @@
+/*
+ * Copyright (c) Sofiia Levinska
+ * This is homework for SoftServe CrashCourse "Automation QC Java 2020"
+ */
+
 package HW;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Task1 {
+/**
+ * Task 1 from 19 October 2020
+ *
+ * @author Sofiia Levinska
+ * @version 09 November 2020
+ */
+
+public class Task1Circle {
 
     /**
      * Method for reading Radius value from Console
+     *
+     * @return Circle radius in double type
      */
-    public static double ReadRadius() throws Exception {
+    private static double ReadRadius() throws Exception {
         System.out.print("Please enter radius: ");
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         double bf_radius = Double.parseDouble(bf.readLine());
@@ -19,43 +33,44 @@ public class Task1 {
     }
 
     /**
-     * Class Circle for creating Object Circle
+     * Class Circle for calculating Circle Perimeter and area
      */
-    public static class Circle {
-        private final double PI = 3.14;
+    private static class Circle {
+        private static final double PI = 3.14;
         private final double radius;
 
         /**
-         * Circle Radius Setter
+         * Circle Constructor sets radius
+         *
+         * @param radius is a Circle radius.
          */
-        public Circle(double radius) {
+        private Circle(double radius) {
             this.radius = radius;
         }
 
         /**
          * Method for calculating Circle Perimeter
          */
-        public String Perimeter() {
+        private String CalculateCirclePerimeter() {
             return String.format("%.2f", 2 * PI * radius);
         }
 
         /**
          * Method for calculating Circle Area
          */
-        public String Area() {
+        private String CalculateCircleArea() {
             return String.format("%.2f", PI * radius * radius);
         }
     }
-
 
     public static void main(String[] args) {
         try {
             double radius = ReadRadius();
             Circle circle = new Circle(radius);
-            System.out.println("Circle perimeter is " + circle.Perimeter());
-            System.out.println("Circle area is " + circle.Area());
+            System.out.println("Circle perimeter is " + circle.CalculateCirclePerimeter());
+            System.out.println("Circle area is " + circle.CalculateCircleArea());
         } catch (Exception e) {
-            System.err.println(e.getMessage()+ ". Please enter radius>=0.");
+            System.err.println(e.getMessage() + ". Please enter radius>=0.");
         }
     }
 }
